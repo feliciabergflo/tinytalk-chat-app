e9affa83078d0b2fd9d0eefcd3ef2e5ba1b27db5
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-28 19:22:49 +0200

    App.js code

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
+import Home from ".components/home/Home";
+import LoggedOut from ".components/loggedOut/LoggedOut";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
+      <Home />
+      <LoggedOut />
     </div>
   );
 }
