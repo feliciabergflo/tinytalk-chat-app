b6cd154bfa356785730a8e40a022b7e531b7104b
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-28 19:42:04 +0200

    Added icons

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Home from "./components/home/Home";
+//import Home from "./components/home/Home";
 import LoggedOut from "./components/loggedOut/LoggedOut";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <LoggedOut />
     </div>
   );
