9fed8493c22249a9a0534c114e204d9b2b64fc2e
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-28 20:02:01 +0200

    Merged background

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 //import Home from "./components/home/Home";
 import LoggedOut from "./components/loggedOut/LoggedOut";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/css/Common.css";
 
 function App() {
   return (
--- chat-app/src/components/loggedOut/LoggedOut.js
+++ chat-app/src/components/loggedOut/LoggedOut.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../css/LoggedOut.css";
+//import "../css/LoggedOut.css";
 import {
     Container,
     Tab,
